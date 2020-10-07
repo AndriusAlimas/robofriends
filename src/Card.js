d4796bcc7f7a659a500e0b16f0ee9713271f5d11
AndriusAlimas <alimaswebdeveloper@gmail.com>
2020-10-07 13:38:25 +0100

    Card component now can accept props from robots.js which are array of robots objects

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-const Card = () => {
+const Card = ({ name, email, id }) => {
   return (
-    <div>
-      <img alt="robots" src="" />
+    <div className="bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5 tc">
+      <img alt="robots" src={`https://robohash.org/${id}?size=200x200`} />
       <div>
-        <h2>Andrius Alimas</h2>
-        <p>andriusjavait@gmail.com</p>
+        <h2>{name}</h2>
+        <p>{email}</p>
       </div>
     </div>
   );
