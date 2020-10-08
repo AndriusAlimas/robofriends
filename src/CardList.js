8f92a85950b6511b7a5e5a3573e392bb750087e1
AndriusAlimas <alimaswebdeveloper@gmail.com>
2020-10-08 10:07:40 +0100

    Deleted some file that not required for this project at the momment. As well index.css styling was deleted. Improved CardList component using deconstructing props for cleaner code, returning cards components using map method

--- src/CardList.js
+++ src/CardList.js
@@ -4,9 +4,16 @@ import Card from "./Card";
 const CardList = ({ robots }) => {
   return (
     <div>
-      <Card id={robots[0].id} name={robots[0].name} email={robots[0].email} />
-      <Card id={robots[1].id} name={robots[1].name} email={robots[1].email} />
-      <Card id={robots[2].id} name={robots[2].name} email={robots[2].email} />
+      {robots.map((user, index) => {
+        return (
+          <Card
+            key={index}
+            id={robots[index].id}
+            name={robots[index].name}
+            email={robots[index].email}
+          />
+        );
+      })}
     </div>
   );
 };
