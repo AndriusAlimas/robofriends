f8b1e925cddd5fef0e4274baa8435f818a24308b
AndriusAlimas <alimaswebdeveloper@gmail.com>
2020-10-19 14:29:33 +0100

    Deleted robots.js file because we dont need it , for this reason we updated code that we fetch json from url and destructive to objects. Then we added life cycle hooks methods to see in action

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
-import { robots } from "./robots";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield: "",
     };
   }
 
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users').
+    then(response=> response.json())
+    .then(users => this.setState({robots : users}))
+  }
   onSearchChange = (event) => {
     this.setState({ searchfield: event.target.value });
   };
@@ -24,6 +28,10 @@ class App extends Component {
         .includes(this.state.searchfield.toLowerCase());
     });
 
+    if (this.state.robots.length === 0){
+      return <h1>Loading....</h1>
+    }else{
+
     return (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
@@ -31,6 +39,7 @@ class App extends Component {
         <CardList robots={filteredRobots} />
       </div>
     );
+    }
   }
 }
 export default App;
