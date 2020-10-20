02555996ef09822986b25f3f5c60c93a65f1622b
AndriusAlimas <alimaswebdeveloper@gmail.com>
2020-10-20 11:23:15 +0100

    Organized files, organized code in App.js destructive robots and searchfield for cleaner code, if else statement change to ternary operator for cleaner code.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
-import SearchBox from "./SearchBox";
-import Scroll from "./Scroll";
+import CardList from "../components/CardList";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
-    this.state = {
+    this.state
+     = {
       robots: [],
       searchfield: "",
     };
@@ -23,17 +24,16 @@ class App extends Component {
   };
 
   render() {
-    const filteredRobots = this.state.robots.filter((robot) => {
+    const {robots , searchfield} = this.state;
+    const filteredRobots = robots.filter((robot) => {
       return robot.name
         .toLowerCase()
-        .includes(this.state.searchfield.toLowerCase());
+        .includes(searchfield.toLowerCase());
     });
 
-    if (this.state.robots.length === 0){
-      return <h1>Loading....</h1>
-    }else{
-
-    return (
+   return !robots.length ? // same as robots.length === 0
+     <h1>Loading....</h1> :
+    (
       <div className="tc">
         <h1 className="f1">RoboFriends</h1>
         <SearchBox searchChange={this.onSearchChange} />
@@ -42,7 +42,7 @@ class App extends Component {
         </Scroll>
       </div>
     );
-    }
+    
   }
 }
 export default App;
